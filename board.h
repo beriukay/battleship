f651dab4ae3075b6f751bfe207949b4e443a003a
beriukay <beriukay+github@gmail.com>
2014-03-26 04:37:40 -0800

    refactored drawBoard into board class, moved all includes here

--- board.h
+++ board.h
@@ -1,24 +1,15 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include "ship.h"
 #include <vector>
 using std::vector;
 #include <string>
 using std::string;
+#include <iostream>
+using std::cout;
+using std::endl;
 
-struct coordinates
-{
-	int row;
-	int column;
-};
-
-struct ship
-{
-	string name;
-	int size;
-	coordinates bow; //If you don't know nautical terms you shouldn't be reading this code.
-	char direction;
-};
 
 class Board
 {
@@ -30,13 +21,14 @@ private:
 public:
 
 	Board(int len=10, int width=10);
-	void populateBoard(vector<ship> & ships);
-	void fillLeft(ship currentShip) { fillLR(currentShip, -1); }
-	void fillRight(ship currentShip) { fillLR(currentShip, 1); }
-	void fillLR(ship theShip, int dir);
-	void fillUp(ship currentShip) { fillUD(currentShip, -1); }
-	void fillDown(ship currentShip) { fillUD(currentShip, 1); }
-	void fillUD(ship theShip, int dir);
+	void populateBoard(vector<Ship> & ships);
+	void fillLeft(Ship currentShip) { fillLR(currentShip, -1); }
+	void fillRight(Ship currentShip) { fillLR(currentShip, 1); }
+	void fillLR(Ship theShip, int dir);
+	void fillUp(Ship currentShip) { fillUD(currentShip, -1); }
+	void fillDown(Ship currentShip) { fillUD(currentShip, 1); }
+	void fillUD(Ship theShip, int dir);
+    void drawBoard();
 	int getBoardWidth() { return _boardWidth; }
 	int getBoardHeight() { return _boardHeight; }
 	vector<char> getBoard() { return _board; }
