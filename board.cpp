89cef4b4e2c5a88742b016864c2a6f1f7cd6254b
beriukay <beriukay+github@gmail.com>
2014-03-26 04:37:10 -0800

    refactored drawBoard into board class

--- board.cpp
+++ board.cpp
@@ -1,16 +1,11 @@
 #include "board.h"
-#include <vector>
-using std::vector;
-#include <string>
-using std::string;
-#include <iostream>
 
 Board::Board(int height, int width) : _boardWidth(width), _boardHeight(height)
 {
 	_board.resize(_boardHeight * _boardWidth, '.');
 }
 
-void Board::populateBoard(vector<ship> &ships)
+void Board::populateBoard(vector<Ship> &ships)
 {
 	for (size_t ii = 0; ii < ships.size(); ++ii)
 	{
@@ -25,7 +20,7 @@ void Board::populateBoard(vector<ship> &ships)
 	}
 }
 
-void Board::fillLR(ship theShip, int dir)
+void Board::fillLR(Ship theShip, int dir)
 {
 	for (int spot = 0; spot < theShip.size; ++spot)
 	{
@@ -34,11 +29,23 @@ void Board::fillLR(ship theShip, int dir)
 	}
 }
 
-void Board::fillUD(ship theShip, int dir)
+void Board::fillUD(Ship theShip, int dir)
 {
 	for (int spot = 0; spot < theShip.size; ++spot)
 	{
 		_board[(theShip.bow.row + (dir * spot)) *_boardWidth + theShip.bow.column] =
 			theShip.name[0];
+	}
+}
+
+void Board::drawBoard()
+{
+	for(int i=0; i < _boardHeight; ++i)
+	{
+		for (int j=0; j < _boardWidth; ++j) 
+		{
+			cout << _board[_boardWidth * i + j] << " ";
+		}
+		cout << endl;
 	}
 }
